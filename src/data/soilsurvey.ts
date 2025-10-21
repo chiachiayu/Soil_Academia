4896f6c65bfc3d467c28e7206e35451a88dc1e8c
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2025-10-21 16:39:32 +0900

    add all

--- src/data/soilsurvey.ts
+++ src/data/soilsurvey.ts
@@ -6,8 +6,12 @@ export const soilsurvey = [
     description: 'Tropical humid paddy soils with plinthite.',
     image: '/images/Taoyuan_1.jpeg',  // ✅ 圖片路徑
     detail: {
-      subtitle: 'Detailed Soil Profile',
-      text: 'This soil profile showed very coarse Mn nodules between the depth of 50-65 cm. The upper layer exhibited fine platy structure due to periodic wetting and drying.',
+      subtitle: 'Detailed Descriptions',
+       text: `
+		This soil profile showed coarse Fe—Mn nodules between the depth of 50—65 cm.
+		The upper layer exhibited fine platy structure due to periodic wetting and drying.
+		Fe—Mn nodules reflect clear evidence of soil chemistry; that is,
+		Mn always moved faster than Fe during flooding due to its higher redox potential.`,
       subimages: ['/images/Taoyuan_2.png', '/images/Taoyuan_3.png'],  // 可選，若要附上細節圖
     },
   },

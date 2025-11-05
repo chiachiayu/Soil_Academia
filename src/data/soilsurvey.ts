f834dfdc56d843fb1e387ec00423c0f2dd649dba
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2025-11-05 11:35:24 +0900

    add penghu soils

--- src/data/soilsurvey.ts
+++ src/data/soilsurvey.ts
@@ -17,11 +17,11 @@ export const soilsurvey = [
   },
 
     {
-    location: '桃園 (Taoyuan), Taiwan',
-    time: '2025/04/01 - 2025/04/02',
-    soilclassification: 'Typic Hapludult',
-    description: 'Tropical humid paddy soils with plinthite.',
-    image: '/images/Taoyuan_1.jpeg',  // ✅ 圖片路徑
+    location: '澎湖 (Penghu), Taiwan',
+    time: '2017/04/23',
+    soilclassification: 'Typic Haplustalfs',
+    description: 'Tropical humid basaltic soils.',
+    image: '/images/soilsurvey/Penghu_1.png',  // ✅ 圖片路徑
     detail: {
       subtitle: 'Detailed Descriptions',
        text: `
@@ -29,7 +29,7 @@ export const soilsurvey = [
 		The upper layer exhibited fine platy structure due to periodic wetting and drying.
 		Fe—Mn nodules reflect clear evidence of soil chemistry; that is,
 		Mn always moved faster than Fe during flooding due to its higher redox potential.`,
-      subimages: ['/images/Taoyuan_2.png', '/images/Taoyuan_3.png'],  // 可選，若要附上細節圖
+      subimages: ['/images/Penghu_2.jpg', '/images/Penghu_3.tif'],  // 可選，若要附上細節圖
     },
   }
 ];

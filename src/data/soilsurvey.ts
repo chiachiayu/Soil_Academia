5434677fb0a9648415bc31177ce0ef612a94ebec
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2025-11-05 11:15:58 +0900

    modify survey

--- src/data/soilsurvey.ts
+++ src/data/soilsurvey.ts
@@ -15,4 +15,21 @@ export const soilsurvey = [
       subimages: ['/images/Taoyuan_2.png', '/images/Taoyuan_3.png'],  // 可選，若要附上細節圖
     },
   },
+
+    {
+    location: '桃園 (Taoyuan), Taiwan',
+    time: '2025/04/01 - 2025/04/02',
+    soilclassification: 'Typic Hapludult',
+    description: 'Tropical humid paddy soils with plinthite.',
+    image: '/images/Taoyuan_1.jpeg',  // ✅ 圖片路徑
+    detail: {
+      subtitle: 'Detailed Descriptions',
+       text: `
+		This soil profile showed coarse Fe—Mn nodules between the depth of 50—65 cm.
+		The upper layer exhibited fine platy structure due to periodic wetting and drying.
+		Fe—Mn nodules reflect clear evidence of soil chemistry; that is,
+		Mn always moved faster than Fe during flooding due to its higher redox potential.`,
+      subimages: ['/images/Taoyuan_2.png', '/images/Taoyuan_3.png'],  // 可選，若要附上細節圖
+    },
+  }
 ];

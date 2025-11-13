f43981c77068cc74e2fe3870201e2c4cf0c8d4b2
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2025-11-13 16:00:03 +0900

    change all

--- src/data/soilsurvey.ts
+++ src/data/soilsurvey.ts
@@ -4,7 +4,7 @@ export const soilsurvey = [
     time: '2025/01/03',
     soilclassification: 'Typic Hapludults',
     description: 'Tropical humid paddy soils with plinthite.',
-    image: '/images/Taoyuan_1.jpeg',  // ✅ 圖片路徑
+    image: '/images/soilsurvey/Taoyuan/Taoyuan_1.jpeg',  // ✅ 圖片路徑
     detail: {
       subtitle: 'Detailed Descriptions',
        text: `
@@ -12,7 +12,7 @@ export const soilsurvey = [
 		The upper layer exhibited fine platy structure due to periodic wetting and drying.
 		Fe—Mn nodules reflect clear evidence of soil chemistry; that is,
 		Mn always moved faster than Fe during flooding due to its higher redox potential.`,
-      subimages: ['/images/Taoyuan_2.png', '/images/Taoyuan_3.png'],  // 可選，若要附上細節圖
+      subimages: ['/images/soilsurvey/Taoyuan/Taoyuan_2.png', '/images/soilsurvey/Taoyuan/Taoyuan_3.png'],  // 可選，若要附上細節圖
     },
   },
 
@@ -21,12 +21,12 @@ export const soilsurvey = [
     time: '2017/04/23',
     soilclassification: 'Typic Haplustalfs',
     description: 'Tropical humid basaltic soils.',
-    image: '/images/soilsurvey/Penghu_1.png',  // ✅ 圖片路徑
+    image: '/images/soilsurvey/Penghu/Penghu_1.png',  // ✅ 圖片路徑
     detail: {
       subtitle: 'Detailed Descriptions',
        text: `
 		None.`,
-      subimages: ['/images/soilsurvey/Penghu_2.jpg', '/images/soilsurvey/Penghu_3.tif'],  // 可選，若要附上細節圖
+      subimages: ['/images/soilsurvey/Penghu/Penghu_2.jpg', '/images/soilsurvey/Penghu/Penghu_3.tif'],  // 可選，若要附上細節圖
     },
   }
 ];

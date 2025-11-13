a6e2ec47751293bba7729d8c5a9208279c76d37d
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2025-11-13 15:54:00 +0900

    add conference

--- src/data/Academicactivity.ts
+++ src/data/Academicactivity.ts
@@ -17,4 +17,32 @@ export const activity = [
       ],  // 可選，若要附上細節圖
     },
   },
+  {
+    location: '京都 (Kyoto), Japan',
+    time: '2025/06/13 - 2025/06/17',
+    name: 'ICSE2025',
+    description: '11th International Conference on Serpentine Ecology',
+    image: '/images/icse2025/icse2025_home.png',  // ✅ 圖片路徑
+    detail: {
+      subtitle: 'More about the activity',
+       text: `
+		    In June 2025, Dr. Chia-Yu Yang attended the 11th International Conference on Serpentine Ecology (ICSE2025) in Kyoto, Japan.`,
+      subimages: ['/images/icse2025/icse2025_1.png', '/images/icse2025/icse2025_2.png', '/images/icse2025/icse2025_3.png', '/images/icse2025/icse2025_4.png', '/images/icse2025/icse2025_5.png'
+      ],  // 可選，若要附上細節圖
+    },
+  },
+  {
+    location: '新潟 (Niigata), Japan',
+    time: '2025/09/17 - 2025/09/19',
+    name: 'JSSSPN2025',
+    description: '日本土壌肥料学会2025年度新潟大会',
+    image: '/images/jssspn2025/jssspn2025_home.png',  // ✅ 圖片路徑
+    detail: {
+      subtitle: 'More about the activity',
+       text: `
+		    In September 2025, Dr. Chia-Yu Yang attended the 2025 Annual Meeting of the Japanese Society of Soil Science and Plant Nutrition (JSSSPN2025) in Niigata, Japan.`,
+      subimages: ['/images/jssspn2025/jssspn2025_1.jpg', '/images/jssspn2025/jssspn2025_2.jpg', '/images/jssspn2025/jssspn2025_3.png', '/images/jssspn2025/jssspn2025_4.png', '/images/jssspn2025/jssspn2025_5.png'
+      ],  // 可選，若要附上細節圖
+    },
+  },
 ];

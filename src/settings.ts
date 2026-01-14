dfb6fc8dc3c4337c2b24b7bbc32da7141b63810e
chiachiayu <105585698+chiachiayu@users.noreply.github.com>
2026-01-14 15:53:52 +0900

    change

--- src/settings.ts
+++ src/settings.ts
@@ -38,5 +38,5 @@ export const template = {
 export const seo = {
 	default_title: 'Chia-Yu Yang - Academic Portfolio',
 	default_description: 'Chia-Yu Yang\'s academic portfolio showcasing research in soil science and environmental geochemistry.',
-	default_image: '/images/webicon.jpg',
+	default_image: '/images/webicon.png',
 }
